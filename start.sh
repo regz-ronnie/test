8a86dddc2da91f91493e7fe19ce1378cfe9ba453
Ronald Kiprotich <ronalkipro18@gmail.com>
2025-11-07 01:43:50 +0300

    Fix gunicorn path to backend.backend.wsgi

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Start Gunicorn for Django
 echo "Starting Gunicorn..."
 cd /opt/render/project/src/backend
-exec gunicorn test.wsgi:application \
+exec gunicorn backend.backend.wsgi:application \
     --bind 0.0.0.0:$PORT \
     --workers 3 \
     --pythonpath /opt/render/project/src/backend
